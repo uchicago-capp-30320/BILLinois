62dcac9d400b3699418183b7fb00069ab668dc40
Caitlin Pratt <cpratt@swingleft.org>
2025-04-18 13:43:34 -0500

    Test file

---
+++ test/test.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello from billinois!")
+    print("A project by Livia Mucciolo, Echo Nattinger, Joaquin Pinto , " \
+    "Caitlin Pratt , David Steffen, Suchi Tailor, and Karen Yi")
+
+
+if __name__ == "__main__":
+    main()
