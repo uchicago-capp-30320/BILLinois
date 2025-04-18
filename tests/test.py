eee8435472e2ac27df36fac39106dfcb8e7d7a08
David Steffen <david.steffen@pomona.edu>
2025-04-18 14:08:08 -0500

    #22 adding license file

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,9 @@
 def main():
     print("Hello from billinois!")
-    print("A project by Livia Mucciolo, Echo Nattinger, Joaquin Pinto , " \
-    "Caitlin Pratt , David Steffen, Suchi Tailor, and Karen Yi")
+    print(
+        "A project by Livia Mucciolo, Echo Nattinger, Joaquin Pinto , "
+        "Caitlin Pratt , David Steffen, Suchi Tailor, and Karen Yi"
+    )
 
 
 if __name__ == "__main__":
