8f311cc0531f729f0a6b17eb47df5e8eb32a3bf0
Caitlin Pratt <cpratt@swingleft.org>
2025-04-21 22:29:41 -0500

    Add initial migrations and update config

---
+++ apps/core/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+
+
+class actions_mock_django(models.Model):
+    """
+    A mock model for the actions table.
+    Meant to store mock data for actions taken on legislation.
+    """
+
+    bill_id = models.ForeignKey("bills_mock_django", on_delete=models.CASCADE)
+    action_id = models.CharField(unique=True, primary_key=True)
+    description = models.CharField()
+    date = models.DateTimeField()
+
+    class Meta:
+        db_table = "actions_mock_django"  # Specify table name
+
+
+class bills_mock_django(models.Model):
+    """
+    A mock model for the bills table.
+    Meant to store mock data for bills.
+    """
+
+    bill_id = models.CharField(unique=True, primary_key=True)
+    number = models.CharField()
+    title = models.CharField()
+    summary = models.CharField()
+    status = models.CharField()
+
+    class Meta:
+        db_table = "bills_mock_django"
+
+
+class favorites_mock_django(models.Model):
+    """
+    A mock model for the favorites table.
+    Meant to store mock data for user favorites.
+    """
+
+    user_id = models.ForeignKey("users_mock_django", on_delete=models.CASCADE)
+    bill_id = models.ForeignKey("bills_mock_django", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "favorites_mock_django"
+
+
+class sponsors_mock_django(models.Model):
+    """
+    A mock model for the sponsors table.
+    Meant to store mock data for bill sponsors.
+    """
+
+    id = models.CharField(unique=True, primary_key=True)
+    bill_id = models.ForeignKey("bills_mock_django", on_delete=models.CASCADE)
+    sponsor_id = models.CharField()
+    sponsor_name = models.CharField()
+
+    class Meta:
+        db_table = "sponsors_mock_django"
+
+
+class topics_mock_django(models.Model):
+    """
+    A mock model for the topics table.
+    Meant to store mock data for bill topics.
+    """
+
+    bill_id = models.ForeignKey("bills_mock_django", on_delete=models.CASCADE)
+    topic = models.CharField()
+
+    class Meta:
+        db_table = "topics_mock_django"
+
+
+class users_mock_django(models.Model):
+    """
+    A mock model for the users table.
+    Meant to store mock data for users.
+    """
+
+    user_id = models.CharField(unique=True, primary_key=True, null=False)
+    password = models.CharField()
+    phone = models.CharField()
+    zip = models.CharField()
+
+    class Meta:
+        db_table = "users_mock_django"
