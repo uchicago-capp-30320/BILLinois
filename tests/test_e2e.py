54f9957c70990d88c63e0431b6f03b3b41575772
Karen <notkarenyi@gmail.com>
2025-05-04 16:34:48 -0500

    test home page

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -7,8 +7,14 @@ def test_has_title(page: Page):
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
 
-def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
+def test_home_exists(page: Page):
+    
+    page.goto("http://127.0.0.1:8000/")
+
+    # expect title to contain "BILL"
+    # expect(page).to_have_title(re.compile("ois"))
+
+    expect(page.locator("h1")).to_have_text(re.compile("ois"))
 
     # Click the get started link.
     page.get_by_role("link", name="Get started").click()
