d9186fb6cce5f78d236e307b5cc3b2619737419e
Caitlin Pratt <cpratt@swingleft.org>
2025-05-12 22:05:43 -0500

    Fix error in urls.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("search/", views.search, name="search"),
     path("toggle_favorite/<path:bill_id>/", views.toggle_favorite, name="toggle_favorite"),
+]
