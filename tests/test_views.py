5e1f6c8e1646868f5dce9047d4d33e78c7ba720f
David Steffen <david.steffen@pomona.edu>
2025-05-18 10:59:16 -0500

    test views formatting

--- tests/test_views.py
+++ tests/test_views.py
@@ -8,17 +8,15 @@ from apps.core.models import BillsTable
 def test_home(client):
     return client.get("/")
 
+
 @pytest.mark.django_db
 def test_home_status(test_home):
     assert test_home.status_code == 200
 
+
 @pytest.mark.django_db
 def test_home_content(test_home):
-    bytes = [
-        b"BILLinois",
-        b"Get started by searching a custom bill topic",
-        b"Education"
-    ]
+    bytes = [b"BILLinois", b"Get started by searching a custom bill topic", b"Education"]
     assert all(byte in test_home.content for byte in bytes)
 
 
@@ -27,14 +25,17 @@ def test_home_content(test_home):
 def test_search_fake(client):
     return client.get("/search/", {"query": "This is a test query."})
 
+
 @pytest.mark.django_db
 def test_create_search(test_search_fake):
     assert test_search_fake.status_code == 200
+
 
 @pytest.mark.django_db
 def test_search_content(test_search_fake):
     bytes = [b"BILLinois", b"Menu"]
     assert all(byte in test_search_fake.content for byte in bytes)
+
 
 # Search view tests: real query
 @pytest.fixture
@@ -47,13 +48,16 @@ def test_bill():
         status="Introduced",
     )
 
+
 @pytest.fixture
 def test_search_real(client, test_bill):
     return client.get("/search/", {"query": "Transportation"})
 
+
 @pytest.mark.django_db
 def test_create_search_real(test_search_real):
     assert test_search_real.status_code == 200
+
 
 @pytest.mark.django_db
 def test_search_content_real(test_search_real, test_bill):
@@ -62,7 +66,7 @@ def test_search_content_real(test_search_real, test_bill):
 
 
 # Bill page view tests: real query
-# Not implemented for now as these are not currently in urls.py
+# Not implemented for now
 # @pytest.fixture
 # def test_bill_view(client, test_bill):
 #     return client.get("/bill_page/", {"bill_number": "123"})
