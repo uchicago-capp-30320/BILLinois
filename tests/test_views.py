e2098eafb580223105a700d5e827ef0cd82e0d61
David Steffen <david.steffen@pomona.edu>
2025-05-18 10:24:58 -0500

    update search view tests to incorporate an actual bill searched for

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,23 +1,20 @@
 import pytest
 from django.urls import reverse
+from apps.core.models import BillsTable
 
 
+# Home view tests
 @pytest.fixture
 def test_home(client):
     return client.get("/")
-
 
 @pytest.mark.django_db
 def test_home_status(test_home):
     assert test_home.status_code == 200
 
-
 @pytest.mark.django_db
 def test_home_content(test_home):
     bytes = [
-        # This works in the current branch
-        # b"Billinois"
-        # These will work on dev
         b"BILLinois",
         b"Get started by searching a custom bill topic",
         b"Education"
@@ -25,33 +22,40 @@ def test_home_content(test_home):
     assert all(byte in test_home.content for byte in bytes)
 
 
+# Search view tests: fake query
 @pytest.fixture
 def test_search_fake(client):
     return client.get("/search/", {"query": "This is a test query."})
 
-
 @pytest.mark.django_db
 def test_create_search(test_search_fake):
     assert test_search_fake.status_code == 200
-
 
 @pytest.mark.django_db
 def test_search_content(test_search_fake):
     bytes = [b"BILLinois", b"Menu"]
     assert all(byte in test_search_fake.content for byte in bytes)
 
+# Search view tests: real query
+@pytest.fixture
+def test_bill():
+    return BillsTable.objects.create(
+        bill_id="123",
+        number="AB 123",
+        title="Transportation Test Bill",
+        summary="This is a test bill.",
+        status="Introduced",
+    )
 
 @pytest.fixture
-def test_search_real(client):
+def test_search_real(client, test_bill):
     return client.get("/search/", {"query": "Transportation"})
-
 
 @pytest.mark.django_db
 def test_create_search_real(test_search_real):
     assert test_search_real.status_code == 200
 
-
 @pytest.mark.django_db
-def test_search_content_real(test_search_real):
-    bytes = [b"BILLinois", b"Menu"]
+def test_search_content_real(test_search_real, test_bill):
+    bytes = [b"BILLinois", b"Menu", b"123", b"Transportation Test Bill"]
     assert all(byte in test_search_real.content for byte in bytes)
