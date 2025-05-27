6acfb35ba7795c1015d61f93d817a73956074b23
David Steffen <david.steffen@pomona.edu>
2025-05-27 14:13:53 -0500

    mostly adding documentation but also added a few more views tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,11 +13,13 @@ def test_home(client):
     return client.get("/")
 
 
+# Test that the home page loads
 @pytest.mark.django_db
 def test_home_status(test_home):
     assert test_home.status_code == 200
 
 
+# Test that the home page has the expected content
 @pytest.mark.django_db
 def test_home_content(test_home):
     bytes = [b"BILLinois", b"Get started by searching a custom bill topic", b"Education"]
@@ -30,18 +32,20 @@ def test_search_fake(client):
     return client.get("/search/", {"query": "This is a test query.", "state": "IL"})
 
 
+# Test that the search works
 @pytest.mark.django_db
 def test_create_search(test_search_fake):
     assert test_search_fake.status_code == 200
 
 
+# Test that the result returns nothing as expected
 @pytest.mark.django_db
 def test_search_content(test_search_fake):
-    bytes = [b"BILLinois", b"Menu"]
+    bytes = [b"BILLinois", b"Menu", b"No results found."]
     assert all(byte in test_search_fake.content for byte in bytes)
 
 
-# Search view tests: real query
+# Create test bills
 @pytest.fixture
 def test_bill():
     bill_il = BillsTable.objects.create(
@@ -65,23 +69,32 @@ def test_bill():
     return bill_il, bill_in
 
 
+# Search view tests: real query
 @pytest.fixture
 def test_search_real(client, test_bill):
     return client.get("/search/", {"query": "Transportation"})
 
 
+# Test that the search worked
 @pytest.mark.django_db
 def test_create_search_real(test_search_real):
     assert test_search_real.status_code == 200
 
 
+# Test that the bills are both returned when no state is filtered for
 @pytest.mark.django_db
 def test_search_content_real(test_search_real, test_bill):
-    bytes = [b"BILLinois", b"Menu", b"123", b"Transportation Test Bill", b"Illinois", b"Indiana"]
+    bytes = [
+        b"BILLinois",
+        b"Menu",
+        b"123",
+        b"Transportation Test Bill (Illinois)",
+        b"Transportation Test Bill (Indiana",
+    ]
     assert all(byte in test_search_real.content for byte in bytes)
 
 
-# Test specifically by state
+# Test search specifically by state
 @pytest.fixture
 def test_search_by_state(client, test_bill):
     return client.get("/search/", {"query": "Transportation", "state": "Illinois"})
@@ -93,7 +106,7 @@ def test_create_search_by_state(test_search_by_state):
     assert test_search_by_state.status_code == 200
 
 
-# Test that the filtering works
+# Test that the filtering works - Illinois bill is returned and Indiana bill is not
 @pytest.mark.django_db
 def test_search_content_by_state_real(test_search_by_state, test_bill):
     bytes = [b"BILLinois", b"Menu", b"123", b"Transportation Test Bill (Illinois)"]
@@ -126,9 +139,20 @@ def test_bill_view_id(client, test_bill):
     return response
 
 
+# Test that the bill page is created
 @pytest.mark.django_db
 def test_create_bill_view_by_id(client, test_bill_view_id):
     assert test_bill_view_id.status_code == 200
+
+
+# Test that the bill page has the correct information
+@pytest.mark.django_db
+def test_bill_view_by_id_content(test_bill_view_id):
+    bytes = [b"Transportation Test Bill (Illinois)"]
+    no_bytes = [b"Transportation Test Bill (Indiana)"]
+    assert all(byte in test_bill_view_id.content for byte in bytes) and not all(
+        byte in test_bill_view_id.content for byte in no_bytes
+    )
 
 
 # Bill page view tests: query by state, session, and bill number
@@ -139,17 +163,29 @@ def test_bill_view_info(client, test_bill):
     return response
 
 
+# Test that the bill page is returned
 @pytest.mark.django_db
-def test_create_bill_view(test_bill_view_info):
+def test_create_bill_view_by_info(test_bill_view_info):
     assert test_bill_view_info.status_code == 200
 
 
-# Test the privacy policy works
+# Test that the bill page has the correct information
+@pytest.mark.django_db
+def test_bill_view_by_info_content(test_bill_view_info):
+    bytes = [b"Transportation Test Bill (Illinois)"]
+    no_bytes = [b"Transportation Test Bill (Indiana)"]
+    assert all(byte in test_bill_view_info.content for byte in bytes) and not all(
+        byte in test_bill_view_info.content for byte in no_bytes
+    )
+
+
+# Test privacy policy call
 @pytest.fixture
 def test_privacy_policy(client):
     return client.get("/privacy_policy/")
 
 
+# Test the privacy policy works
 @pytest.mark.django_db
 def test_privacy_policy_works(test_privacy_policy):
     assert test_privacy_policy.status_code == 200
